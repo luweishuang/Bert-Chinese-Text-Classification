99ae7cc8fa9b1676e60ae907821cfdc39ebb7c7c
pifuchan <pifuchan@gmail.com>
2019-12-06 15:30:08 +0800

    debug run

--- Bert-Chinese-Text-Classification-Pytorch/run.py
+++ Bert-Chinese-Text-Classification-Pytorch/run.py
@@ -13,7 +13,7 @@ args = parser.parse_args()
 
 
 if __name__ == '__main__':
-    dataset = 'intent_classify'  # 数据集  
+    dataset = 'intent_classify'  # 数据集
 
     model_name = args.model  # bert
     x = import_module('models.' + model_name)
